a6231d0e52e138ae6863314a10a85add1863b898
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-05 09:58:00 -0700

    Complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import { useState, useEffect } from 'react'
+
+const App = () => {
+    const [dog, setDog] = useState('')
+
+    const handleFetch = async () => {
+        const resp = await fetch('https://dog.ceo/api/breeds/image/random')
+        const data = await resp.json()
+        setDog(data.message)
+    }
+
+    useEffect((handleFetch), [])
+
+    if (!dog) {
+        return <p>Loading ...</p>
+    }
+
+    return (
+        <div>
+            <img src={dog} alt="A Random Dog" /> 
+        </div>
+    )
+}
+
+export default App
